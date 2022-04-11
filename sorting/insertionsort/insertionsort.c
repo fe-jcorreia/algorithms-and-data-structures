fd2b0421fe5443f1ae03b36cf7ab1954c73578c9
fe-jcorreia <fe.jcorreia@gmail.com>
2022-04-11 18:59:19 -0300

    refactoring insertion sort

--- sorting/insertionsort/insertionsort.c
+++ sorting/insertionsort/insertionsort.c
@@ -5,26 +5,14 @@
 
 void insertionsort(Vector *v)
 {
-  int n = v->length;
-
-  for (int sorted_len = 1; sorted_len < n; sorted_len++)
+  for (int key = 1; key < v->length; key++)
   {
-    int key = sorted_len;
-
-    if (v->array[key] >= v->array[key - 1])
-      continue;
-
     int aux = v->array[key];
-    while (key != 0)
+    while (key != 0 && v->array[key - 1] > v->array[key])
     {
-      if (v->array[key - 1] > v->array[key])
-      {
-        v->array[key] = v->array[key - 1];
-        v->array[key - 1] = aux;
-        key--;
-      }
-      else
-        break;
+      v->array[key] = v->array[key - 1];
+      v->array[key - 1] = aux;
+      key--;
     }
   }
 }
