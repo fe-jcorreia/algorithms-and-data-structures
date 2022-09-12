b1903b2c3d5412dda9b1a6ec81002f90945ea957
fe-jcorreia <fe.jcorreia@gmail.com>
2022-09-12 18:40:48 -0300

    dijkstra correction

--- graphs/shortest-path/dijkstra/dijkstra.py
+++ graphs/shortest-path/dijkstra/dijkstra.py
@@ -1,5 +1,4 @@
 import sys
-from heap import Heap
 
 def get_min_distance(distance_list, status):
   min_item = sys.maxsize
@@ -15,13 +14,6 @@ def dijkstra(graph, initialIndex):
   if initialIndex < 0 or initialIndex >= len(graph):
     print("Vértice inválido")
     return
-  
-  heap = Heap()
-  for index, _ in enumerate(graph):
-    if index == initialIndex:
-      heap.insert_on_heap(0, index)
-    else:
-      heap.insert_on_heap(sys.maxsize, index)
 
   total_distance = [sys.maxsize for _ in graph]
   total_distance[initialIndex] = 0
@@ -30,7 +22,7 @@ def dijkstra(graph, initialIndex):
 
   status = [False for _ in graph]
 
-  while(not heap.is_empty_heap()):
+  while(False in status):
     _, currentVertex = get_min_distance(total_distance, status)
 
     for edge in graph[currentVertex]:
