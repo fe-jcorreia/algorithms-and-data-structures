d3de3f949d516ebc941bc7d5e62590e02c7d4729
fe-jcorreia <fe.jcorreia@gmail.com>
2022-09-14 18:21:29 -0300

    prim's algorithm implementation

---
+++ graphs/minimum-spanning-tree/prim/main.py
@@ -0,0 +1,5 @@
+from tests import TestPrim
+
+testPrim = TestPrim()
+
+testPrim.run_all_tests()
---
+++ graphs/minimum-spanning-tree/prim/prim.py
@@ -0,0 +1,37 @@
+import sys
+import heapq
+
+class Prim():
+  def execute(self, graph, initialIndex):
+    if initialIndex < 0 or initialIndex >= len(graph):
+      print("Vértice inválido")
+      return
+
+    frontier_distance = [sys.maxsize for _ in graph]
+    frontier_distance[initialIndex] = 0
+
+    heap = [(0, initialIndex)]
+
+    parent = [None for _ in graph]
+
+    status = [False for _ in graph]
+
+    while(len(heap) > 0):
+      currentDistance, currentVertex = heapq.heappop(heap)
+      status[currentVertex] = True
+
+      if currentDistance > frontier_distance[currentVertex]:
+        continue
+
+      for edge in graph[currentVertex]:
+        nextVertex = edge[1]
+        edgeWeight = edge[2]
+
+        if frontier_distance[nextVertex] > edgeWeight and status[nextVertex] == False:
+          frontier_distance[nextVertex] = edgeWeight
+          heapq.heappush(heap, (frontier_distance[nextVertex], nextVertex))
+          parent[nextVertex] = currentVertex
+
+    print("================ RESULT ================")
+    print("FROM INDEX", initialIndex)
+    print("\nparent: ", parent)
