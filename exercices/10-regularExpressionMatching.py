137dc8976f104933575310cf6c5f35caf9c0b1ec
fe-jcorreia <fe.jcorreia@gmail.com>
2023-04-27 12:15:32 -0300

    dp matrix implementation

--- exercices/10-regularExpressionMatching.py
+++ exercices/10-regularExpressionMatching.py
@@ -1,4 +1,4 @@
-def isMatch(s, p):
+def isMatch_old(s, p):
   m, n = len(s), len(p)
   cache = {}
 
@@ -20,6 +20,20 @@ def isMatch(s, p):
     return False
 
   return dfs(0,0)
+
+def isMatch(s, p):
+  n, m = len(s), len(p)
+  dp = [[False] * (m + 1) for _ in range(n + 1)]
+  dp[0][0] = True
+  
+  for i in range(n + 1):
+    for j in range(1, m + 1):
+      if p[j - 1] == "*":
+        dp[i][j] = dp[i][j - 2] or (i > 0 and (s[i - 1] == p[j - 2] or p[j - 2] == ".") and dp[i - 1][j])
+      else:
+        dp[i][j] = i > 0 and dp[i - 1][j - 1] and (s[i - 1] == p[j - 1] or p[j - 1] == ".")
+  
+  return dp[n][m]
 
 print(isMatch("aa", "a")) # false
 print(isMatch("aa", "a*")) # true
